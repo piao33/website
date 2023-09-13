57046d1d64b2c42ba06df24ca06618e29daf4aaf
朴铭杰 <piaomj3@gmail.com>
2023-09-13 09:12:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import navVue from './components/nav.vue';
+
+</script>
+
+<template>
+    <navVue></navVue>
+    <div class="content">
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+    .content{
+        width: 100%;
+    }
+</style>
