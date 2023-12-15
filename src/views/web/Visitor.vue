b2ba0bc2d801222a912afd64d6eb22bb64c62c5b
朴铭杰 <piaomj3@gmail.com>
2023-12-15 16:00:21 +0800

    keepalive

--- src/views/web/Visitor.vue
+++ src/views/web/Visitor.vue
@@ -1,8 +1,26 @@
 <template>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+        <keep-alive :include="keepAliveRouters">
+            <component :is="Component" ></component>
+        </keep-alive>
+    </router-view>
 </template>
 <script setup>
-    
+import { RouterView } from 'vue-router'
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function calcKeepAliveRouters() {
+    let arr = [];
+    router.getRoutes().forEach(item => {
+        if(item?.meta?.keepAlive) {
+            arr.push(item.meta.componentName);
+        }
+    })
+    return arr;
+}
+let keepAliveRouters = calcKeepAliveRouters();  
 </script>
 <style lang="less" scoped>
    
